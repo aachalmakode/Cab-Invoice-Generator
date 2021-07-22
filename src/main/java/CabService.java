36d88fdcab4362184b178ced79959b06a789faaa
aachalmakode <aachalmakode900@gmail.com>
2021-07-22 16:40:52 -0400

    Added : Multiple rides with total fare

--- src/main/java/CabService.java
+++ src/main/java/CabService.java
@@ -1,18 +1,26 @@
 import java.util.List;
 
 public class CabService {
-    private static final int COST_PER_KM = 10;
-    private static final int COST_PER_MIN = 1;
-    private static final int MINIMUM_COST = 5;
+    private static final int cost_per_km = 10;
+    private static final int  cost_per_min = 1;
+    private static final int min_cost = 5;
+
     public double totalFareOfJourney(double distance, int time) {
-        double cost = (distance*COST_PER_KM) + (time*COST_PER_MIN);
-        if(cost<MINIMUM_COST){
-            return MINIMUM_COST;
+        double cost = (distance*cost_per_km) + (time* cost_per_min);
+        if(cost<min_cost){
+            return min_cost ;
         }
         return cost;
     }
-
+    public double totalFareOfJourney(Rides[] rides) {
+        double fareForMultipleRide = 0;
+        for (Rides ride:rides) {
+            fareForMultipleRide += this.totalFareOfJourney(ride.distance, ride.time);
+        }
+        return fareForMultipleRide;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to CAB INVOICE GENERATOR");
     }
+
 }
--- src/test/java/CabServiceTest.java
+++ src/test/java/CabServiceTest.java
@@ -16,4 +16,18 @@ public class CabServiceTest {
         double totalFare = cabInvoice.totalFareOfJourney(0.1, 2);
         Assert.assertEquals(5, totalFare, 0.0);
     }
+    @Test
+    public void givenDistanceAndTime_whenCostPerKilometrePerMin_shouldReturnTotalFareForMultipleRides() {
+        CabService cabInvoice = new CabService();
+        Rides[] rides ={new Rides(5, 20),
+                new Rides(6, 17),
+                new Rides(4, 20),
+                new Rides(9, 47)};
+        double totalFare = cabInvoice.totalFareOfJourney(rides);
+        Assert.assertEquals(344,totalFare, 0.0);
+
+    }
+
 }
+
+
